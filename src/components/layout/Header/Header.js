c18eb6ab551d1169b51a7d2df5a97df6cd71440b
Barbara <75448241+BarbaraPakula@users.noreply.github.com>
2021-07-18 21:56:30 +0200

    add Header & initialstate

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,19 +1,105 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import clsx from 'clsx';
-
+import styles from './Header.module.scss';
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
-import styles from './Header.module.scss';
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>Header</h2>
-    {children}
-  </div>
-);
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import HomeIcon from '@material-ui/icons/Home';
+import { green } from '@material-ui/core/colors';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Component = ({ className, children }) => {
+  const classes = useStyles();
+  const [log, setLogin] = React.useState();
+  const handleChange = (event) => {
+    setLogin(event.target.checked);
+  };
+
+  return (
+    <div className={clsx(className, styles.root)}>
+      <FormGroup>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={log}
+              onChange={handleChange}
+              aria-label='login switch'
+            />
+          }
+          label={log ? 'Logout' : 'Login'}
+        />
+      </FormGroup>
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton
+            edge='start'
+            className={classes.menuButton}
+            color='inherit'
+            aria-label='menu'
+            href='/'
+          >
+            <HomeIcon style={{ color: green[500] }} />
+          </IconButton>
+          <Typography variant='h6' className={classes.title}>
+            Bulletin
+          </Typography>
+
+          {!log && (
+            <div>
+              <IconButton
+                aria-label='account of current user'
+                aria-controls='menu-appbar'
+                aria-haspopup='true'
+                color='inherit'
+                href='https://google.com'
+              >
+                <AccountCircle /> Login
+              </IconButton>
+            </div>
+          )}
+          {log && (
+            <div>
+              <IconButton
+                aria-label='account of current user'
+                aria-controls='menu-appbar'
+                aria-haspopup='true'
+                color='inherit'
+                href='/'
+              >
+                <AccountCircle />
+                Logout
+              </IconButton>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+
+      {children}
+    </div>
+  );
+};
 
 Component.propTypes = {
   children: PropTypes.node,
